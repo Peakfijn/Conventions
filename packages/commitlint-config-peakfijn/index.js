a05786aaecf3292b6f7b1456c732fe8be8b34a5c
Cedric van Putten <me@bycedric.com>
2018-05-26 21:25:43 +0200

    refactor(commitlint): export imported base configuration instead extend

--- packages/commitlint-config-peakfijn/index.js
+++ packages/commitlint-config-peakfijn/index.js
@@ -1,15 +1,11 @@
 'use strict';
 
 const commitTypes = require('commit-types-peakfijn');
+const config = require('@commitlint/config-conventional');
 
-module.exports = {
-	extends: [
-		'@commitlint/config-conventional'
-	],
-	rules: {
-		'header-max-length': [2, 'always', 80],
-		'scope-empty': [2, 'always'],
-		'subject-min-length': [2, 'always', 20],
-		'type-enum': [2, 'always', Object.keys(commitTypes)],
-	},
-};
+config.rules['header-max-length'] = [2, 'always', 80];
+config.rules['scope-empty'] = [2, 'always'];
+config.rules['subject-min-length'] = [2, 'always', 20];
+config.rules['type-enum'] = [2, 'always', Object.keys(commitTypes)];
+
+module.exports = config;
