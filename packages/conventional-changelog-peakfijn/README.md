70b47773df91887cac31dbb5a1c90988dc7ba6d5
Cedric van Putten <me@bycedric.com>
2018-05-26 20:09:01 +0200

    feature(changelog): add conventional changelog preset with peakfijn types

---
+++ packages/conventional-changelog-peakfijn/README.md
@@ -0,0 +1,3 @@
+# Conventional Changelog - Peakfijn
+
+A [conventional changelog](https://github.com/conventional-changelog/conventional-changelog) preset for [Peakfijn](https://peakfijn.nl/) conventions.
---
+++ packages/conventional-changelog-peakfijn/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const angularPreset = require('conventional-changelog-angular');
+const peakfijnWriterOps = require('./writer-opts');
+const Q = require('q');
+
+module.exports = Q.all([angularPreset, peakfijnWriterOps])
+	.spread(function (convention, writer) {
+		convention.writerOpts = writer;
+		convention.conventionalChangelog.writerOpts = writer;
+
+		return convention;
+	});
