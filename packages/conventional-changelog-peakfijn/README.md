1b388836428a4489fe8716f18fdf030816bd4e63
Cedric van Putten <me@bycedric.com>
2018-05-27 03:42:07 +0200

    refactor(changelog): customise angular convention more
    This will not bail on non-minor-or-higher release types. Instead, all known types are taken into account.

--- packages/conventional-changelog-peakfijn/README.md
+++ packages/conventional-changelog-peakfijn/README.md
@@ -1,3 +1,4 @@
 # Conventional Changelog - Peakfijn
 
 A [conventional changelog](https://github.com/conventional-changelog/conventional-changelog) preset for [Peakfijn](https://peakfijn.nl/) conventions.
+It's actually a slightly customised version of the [angular](https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular) preset.
--- packages/conventional-changelog-peakfijn/writer-opts.js
+++ packages/conventional-changelog-peakfijn/writer-opts.js
@@ -7,14 +7,50 @@ const Q = require('q');
 module.exports = Q.all([angularPreset])
 	.spread(function (convention) {
 		const writer = convention.writerOpts;
-		const writerTransform = convention.writerOpts.transform;
 
 		writer.transform = function (commit, context) {
-			if (commitTypes[commit.type]) {
-				commit.type = commitTypes[commit.type].name;
+			const typeInfo = commitTypes[commit.type];
+
+			if (typeInfo && typeInfo.name) {
+				commit.type = typeInfo.name;
+			} else {
+				return;
 			}
 
-			return writerTransform(commit, context);
+			if (commit.scope === '*') {
+				commit.scope = '';
+			}
+
+			if (typeof commit.hash === 'string') {
+				commit.hash = commit.hash.substring(0, 7);
+			}
+
+			if (typeof commit.subject === 'string') {
+				let url = context.repository
+					? `${context.host}/${context.owner}/${context.repository}`
+					: context.repoUrl;
+
+				if (url) {
+					url = `${url}/issues/`;
+					// Issue URLs.
+					commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+						issues.push(issue);
+
+						return `[#${issue}](${url}${issue})`;
+					});
+				}
+
+				if (context.host) {
+					commit.subject = commit.subject.replace(
+						/\B@([a-z0-9](?:-?[a-z0-9]){0,38})/g,
+						`[@$1](${context.host}/$1)`
+					);
+				}
+			}
+
+			commit.references = commit.references.filter(reference => issues.indexOf(reference.issue) < 0);
+
+			return commit;
 		};
 
 		return writer;
