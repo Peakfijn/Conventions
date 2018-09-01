3eb3f9c68d795a816304948f57d80680333e2139
Cedric van Putten <byCedric@users.noreply.github.com>
2018-09-01 11:41:39 +0200

    fix(release-rules): include default breaking and revert rules (#15)
    Without this breaking changes are not handled as breaking changes. This makes sure both breaking and revert commits are handled properly.

--- packages/release-rules-peakfijn/index.js
+++ packages/release-rules-peakfijn/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const commitTypes = require('commit-types-peakfijn');
+const defaultRules = [
+	{ breaking: true, release: 'major' },
+	{ revert: true, release: 'patch' },
+];
 
-module.exports = Object.keys(commitTypes)
-	.map(type => ({ type, release: commitTypes[type].release }))
-	.filter(rule => !!rule.release);
+module.exports = defaultRules.concat(
+	Object.keys(commitTypes)
+		.map(type => ({ type, release: commitTypes[type].release }))
+		.filter(rule => !!rule.release)
+);
