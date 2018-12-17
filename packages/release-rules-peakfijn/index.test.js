e2647800f1fd8ea41bf079ba9fe1b6a5cb24eb0d
Cedric van Putten <me@bycedric.com>
2018-12-17 16:05:12 +0100

    refactor(semantic-release): add release rules tests, license and documentation

---
+++ packages/release-rules-peakfijn/index.test.js
@@ -0,0 +1,24 @@
+import test from 'ava';
+import types from 'commit-types-peakfijn';
+import rules from './index';
+
+test('breaking changes bumps new major version', t => {
+	t.is(rules.find(rule => rule.breaking).release, 'major');
+});
+
+test('revert changes bumps new patch version', t => {
+	t.is(rules.find(rule => rule.revert).release, 'patch');
+});
+
+test('all commit types are defined with their patch version', t => {
+	Object.keys(types).forEach(type => {
+		const expect = types[type].release;
+		const rule = rules.find(rule => rule.type === type);
+
+		if (expect) {
+			t.is(rule.release, expect);
+		} else {
+			t.falsy(rule);
+		}
+	});
+});
