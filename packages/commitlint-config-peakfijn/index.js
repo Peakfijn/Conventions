b8fa69122152765dc946f7e00523262e339e8406
Cedric van Putten <me@bycedric.com>
2019-02-03 13:38:22 +0100

    refactor(commitlint): lower minimum length of subject to `15` (#88)
    There were some valid comments about the length of the subject. The value `20` came from the original `header-min-length` rule. But this is now changed to `subject-*` only because of the big differences in commit type. I think this will be a welcome change for some people.

--- packages/commitlint-config-peakfijn/index.js
+++ packages/commitlint-config-peakfijn/index.js
@@ -3,7 +3,7 @@ const config = require('@commitlint/config-conventional');
 
 config.rules['header-max-length'] = [2, 'always', 80];
 config.rules['scope-empty'] = [2, 'always'];
-config.rules['subject-min-length'] = [2, 'always', 20];
+config.rules['subject-min-length'] = [2, 'always', 15];
 config.rules['type-enum'] = [2, 'always', Object.keys(commitTypes)];
 
 module.exports = config;
