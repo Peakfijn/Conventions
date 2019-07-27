5ebc9d6566d3092ebf83f6356e02ef87c82299b3
Cedric van Putten <me@bycedric.com>
2019-07-27 10:28:45 +0000

    style: use tabs to indent commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    extends: [
-        './packages/commitlint-config-peakfijn',
-    ],
-    rules: {
-        'scope-empty': [
-            0,
-        ],
-        'scope-enum': [
-            2,
-            'always',
-            [
-                'changelog',
-                'commitizen',
-                'commitlint',
-                'eslint',
-                'semantic-release',
-                'stylelint',
-            ],
-        ],
-    },
-    ignores: [
-        // fix: ignore lockfile updates by greenkeeper
+	extends: [
+		'./packages/commitlint-config-peakfijn',
+	],
+	rules: {
+		'scope-empty': [
+			0,
+		],
+		'scope-enum': [
+			2,
+			'always',
+			[
+				'changelog',
+				'commitizen',
+				'commitlint',
+				'eslint',
+				'semantic-release',
+				'stylelint',
+			],
+		],
+	},
+	ignores: [
+		// fix: ignore lockfile updates by greenkeeper
 		commit => commit.startsWith('chore(package):'),
 	],
 };
